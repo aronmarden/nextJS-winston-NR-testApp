25c8e53c2ec560561ac8d45588fdf0a9a0c13bea
Aron Marden <aronm@penden.com.au>
2024-03-18 23:30:33 +1100

    update: next.config.mjs with shim

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const nrExternals = require('@newrelic/next/load-externals')
 const nextConfig = {
   reactStrictMode: true,
+  webpack: nrExternals 
 };
+
 
 export default nextConfig;
